9ab92449a1747d76ef42afe1776bc0e4bb414a85
HenriquedeSouza21 <h.oliveira2108@gmail.com>
2026-09-14 11:48:17 -0300

    Primeira versao do CRUD Flask

---
+++ app.py
@@ -0,0 +1,65 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///local.db'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Tarefa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(120), nullable=False)
+    descricao = db.Column(db.String(300))
+    concluida = db.Column(db.Boolean, default=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    tarefas = Tarefa.query.all()
+    return render_template('index.html', tarefas=tarefas)
+
+
+@app.route('/nova', methods=['GET', 'POST'])
+def nova_tarefa():
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        descricao = request.form['descricao']
+        nova = Tarefa(titulo=titulo, descricao=descricao)
+        db.session.add(nova)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('form.html', tarefa=None)
+
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar_tarefa(id):
+    tarefa = Tarefa.query.get_or_404(id)
+    if request.method == 'POST':
+        tarefa.titulo = request.form['titulo']
+        tarefa.descricao = request.form['descricao']
+        tarefa.concluida = 'concluida' in request.form
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('form.html', tarefa=tarefa)
+
+
+@app.route('/excluir/<int:id>')
+def excluir_tarefa(id):
+    tarefa = Tarefa.query.get_or_404(id)
+    db.session.delete(tarefa)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
